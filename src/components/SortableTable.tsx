c968958eb3ad087d8456d6e4cda83bf1540a4bfa
Michael Valdes <michael.j.valdes@gmail.com>
2022-10-07 18:03:47 -0400

    Remove unnecessary box around tables

--- src/components/SortableTable.tsx
+++ src/components/SortableTable.tsx
@@ -53,84 +53,80 @@ export function SortableTable({
     });
 
     return (
-        <Box
+        <TableContainer
             border='1px solid'
             borderColor='#E2E8F0'
             borderRadius='md'
             backgroundColor='white'
         >
-            <TableContainer>
-                <Table>
-                    <Thead>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <Tr key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => {
-                                    const meta: any =
-                                        header.column.columnDef.meta;
-                                    return (
-                                        <Th
-                                            key={header.id}
-                                            onClick={header.column.getToggleSortingHandler()}
-                                            isNumeric={meta?.isNumeric}
-                                            {...getHeaderProps(header)}
-                                            role={'button'}
-                                        >
-                                            {flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
+            <Table>
+                <Thead>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <Tr key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                                const meta: any = header.column.columnDef.meta;
+                                return (
+                                    <Th
+                                        key={header.id}
+                                        onClick={header.column.getToggleSortingHandler()}
+                                        isNumeric={meta?.isNumeric}
+                                        {...getHeaderProps(header)}
+                                        role={'button'}
+                                    >
+                                        {flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
 
-                                            <chakra.span pl='4'>
-                                                {header.column.getIsSorted() ? (
-                                                    header.column.getIsSorted() ===
-                                                    'desc' ? (
-                                                        <TriangleDownIcon aria-label='sorted descending' />
-                                                    ) : (
-                                                        <TriangleUpIcon aria-label='sorted ascending' />
-                                                    )
+                                        <chakra.span pl='4'>
+                                            {header.column.getIsSorted() ? (
+                                                header.column.getIsSorted() ===
+                                                'desc' ? (
+                                                    <TriangleDownIcon aria-label='sorted descending' />
                                                 ) : (
-                                                    <UpDownIcon />
-                                                )}
-                                            </chakra.span>
-                                        </Th>
-                                    );
-                                })}
-                            </Tr>
-                        ))}
-                    </Thead>
-                    <Tbody>
-                        {table.getRowModel().rows.map((row) => (
-                            <Tr
-                                key={row.id}
-                                {...getRowProps(row)}
-                                role={
-                                    getRowProps(row).onClick !== undefined
-                                        ? 'button'
-                                        : undefined
-                                }
-                            >
-                                {row.getVisibleCells().map((cell) => {
-                                    const meta: any =
-                                        cell.column.columnDef.meta;
-                                    return (
-                                        <Td
-                                            key={cell.id}
-                                            isNumeric={meta?.isNumeric}
-                                            {...getCellProps(cell)}
-                                            {...getColumnProps(cell.column)}
-                                        >
-                                            {flexRender(
-                                                cell.column.columnDef.cell,
-                                                cell.getContext()
+                                                    <TriangleUpIcon aria-label='sorted ascending' />
+                                                )
+                                            ) : (
+                                                <UpDownIcon />
                                             )}
-                                        </Td>
-                                    );
-                                })}
-                            </Tr>
-                        ))}
-                    </Tbody>
-                </Table>
-            </TableContainer>
-        </Box>
+                                        </chakra.span>
+                                    </Th>
+                                );
+                            })}
+                        </Tr>
+                    ))}
+                </Thead>
+                <Tbody>
+                    {table.getRowModel().rows.map((row) => (
+                        <Tr
+                            key={row.id}
+                            {...getRowProps(row)}
+                            role={
+                                getRowProps(row).onClick !== undefined
+                                    ? 'button'
+                                    : undefined
+                            }
+                        >
+                            {row.getVisibleCells().map((cell) => {
+                                const meta: any = cell.column.columnDef.meta;
+                                return (
+                                    <Td
+                                        key={cell.id}
+                                        isNumeric={meta?.isNumeric}
+                                        {...getCellProps(cell)}
+                                        {...getColumnProps(cell.column)}
+                                    >
+                                        {flexRender(
+                                            cell.column.columnDef.cell,
+                                            cell.getContext()
+                                        )}
+                                    </Td>
+                                );
+                            })}
+                        </Tr>
+                    ))}
+                </Tbody>
+            </Table>
+        </TableContainer>
     );
 }
